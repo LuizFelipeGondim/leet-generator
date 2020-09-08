d2c71e4c4320433993615298717fd8cdceb1e98b
LuizFelipeGondim <luizfelipegondimaraujo@gmail.com>
2020-09-07 22:11:42 -0300

    structure finalized

--- index.php
+++ index.php
@@ -1,13 +1,38 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Leet Generator</title>
+    </head>
+    <body>
+        
+    <h1>Leet Generator</h1>
+    <form method="post">
+        <label for="string">Informe uma frase:</label>
+        <input type="text" name="string" id="string">
+        <button type="submit" name="acao">Transformar</button>
+    </form>
 
-    require 'values.php';
+    <?php
 
-    $string = "A vizinha gosta de chá com biscoito";
+        require 'values.php';
 
-    $string = mb_strtolower($string);
+        if(isset($_POST['acao'])){
+            $string = mb_strtolower($_POST['string']);
 
-    foreach($setValues as $key => $value){
-        $string = str_replace($key, $value, $string);
-    }
+            foreach($setValues as $key => $value){
+                $string = str_replace($key, $value, $string);
+            }
 
-    echo $string;
+            echo $string;
+        }
+    ?>
+
+
+
+
+
+    </body>
+</html>
+
